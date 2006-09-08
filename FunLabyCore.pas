9d5790721b8d122384e5fc38c7b376feeac5ef29
Sébastien Doeraene <sjrdoeraene@gmail.com>
2006-09-08 11:46:00 +0000

    Ajout de quelques cases originales.

--- FunLabyCore.pas
+++ FunLabyCore.pas
@@ -15,27 +15,71 @@ uses
 resourcestring
   sGrass = 'Herbe';                           /// Nom de l'herbe
   sWall = 'Mur';                              /// Nom du mur
+  sWater = 'Eau';                             /// Nom de l'eau
+  sHole = 'Trou';                             /// Nom du trou
+  sSilverBlock = 'Bloc en argent';            /// Nom du bloc en argent
+  sGoldenBlock = 'Bloc en or';                /// Nom du bloc en or
+  sSky = 'Ciel';                              /// Nom du ciel
 
+  sNorthArrow = 'Flèche nord';                /// Nom de la flèche nord
+  sEastArrow = 'Flèche est';                  /// Nom de la flèche est
+  sSouthArrow = 'Flèche sud';                 /// Nom de la flèche sud
+  sWestArrow = 'Flèche ouest';                /// Nom de la flèche ouest
+  sTransporter = 'Téléporteur';               /// Nom du téléporteur
+  sUpStairs = 'Escalier montant';             /// Nom de l'escalier montant
+  sDownStairs = 'Escalier descendant';        /// Nom de l'escalier descendant
   sDirectTurnstile = 'Tourniquet direct';     /// Nom du tourniquet direct
   sIndirectTurnstile = 'Tourniquet indirect'; /// Nom du tourniquet indirect
   sOutside = 'Dehors';                        /// Nom du dehors
 
 const {don't localize}
-  idGrass = 'Grass';                          /// ID de l'herbe
-  idWall = 'Wall';                            /// ID du mur
+  idGrass = 'Grass';                             /// ID de l'herbe
+  idWall = 'Wall';                               /// ID du mur
+  idWater = 'Water';                             /// ID de l'eau
+  idHole = 'Hole';                               /// ID du trou
+  idSilverBlock = 'SilverBlock';                 /// ID du bloc en argent
+  idGoldenBlock = 'GoldenBlock';                 /// ID du bloc en or
+  idSky = 'Sky';                                 /// ID du ciel
 
-  idNoEffect = 'NoEffect';                    /// ID du sans effet
-  idDirectTurnstile = 'DirectTurnstile';      /// ID du tourniquet direct
-  idIndirectTurnstile = 'IndirectTurnstile';  /// ID du tourniquet indirect
-  idOutside = 'Outside';                      /// ID du dehors
+  idNoEffect = 'NoEffect';                       /// ID du sans effet
+  idNorthArrow = 'NorthArrow';                   /// ID de la flèche nord
+  idEastArrow = 'EastArrow';                     /// ID de la flèche est
+  idSouthArrow = 'SouthArrow';                   /// ID de la flèche sud
+  idWestArrow = 'WestArrow';                     /// ID de la flèche ouest
+  idInactiveTransporter = 'InactiveTransporter'; /// ID du téléporteur inactif
+  idTransporterNext = 'TransporterNext';         /// ID du téléporteur suivant
+  idTransporterPrev = 'TransporterPrev';         /// ID du téléporteur précédent
+  idTransporterRandom = 'TransporterRandom';     /// ID du téléporteur aléatoire
+  idUpStairs = 'UpStairs';                       /// ID de l'escalier montant
+  idDownStairs = 'DownStairs';                   /// ID de l'escalier descendant
+  idDirectTurnstile = 'DirectTurnstile';         /// ID du tourniquet direct
+  idIndirectTurnstile = 'IndirectTurnstile';     /// ID du tourniquet indirect
+  idOutside = 'Outside';                         /// ID du dehors
 
 const {don't localize}
-  fGrass = 'Grass';                           /// Fichier de l'herbe
-  fWall = 'Wall';                             /// Fichier du mur
+  fGrass = 'Grass';                         /// Fichier de l'herbe
+  fWall = 'Wall';                           /// Fichier du mur
+  fWater = 'Water';                         /// Fichier de l'eau
+  fHole = 'Hole';                           /// Fichier du trou
+  fSilverBlock = 'SilverBlock';             /// Fichier du bloc en argent
+  fGoldenBlock = 'GoldenBlock';             /// Fichier du bloc en or
+  fSky = 'Sky';                             /// Fichier du ciel
 
-  fDirectTurnstile = 'DirectTurnstile';       /// Fichier du tourniquet direct
-  fIndirectTurnstile = 'IndirectTurnstile';   /// Fichier du tourniquet indirect
-  fOutside = 'Outside';                       /// Fichier du dehors
+  fNorthArrow = 'NorthArrow';               /// Fichier de la flèche nord
+  fEastArrow = 'EastArrow';                 /// Fichier de la flèche est
+  fSouthArrow = 'SouthArrow';               /// Fichier de la flèche sud
+  fWestArrow = 'WestArrow';                 /// Fichier de la flèche ouest
+  fTransporter = 'Transporter';             /// Fichier du téléporteur inactif
+  fUpStairs = 'UpStairs';                   /// Fichier de l'escalier montant
+  fDownStairs = 'DownStairs';               /// Fichier de l'escalier descendant
+  fDirectTurnstile = 'DirectTurnstile';     /// Fichier du tourniquet direct
+  fIndirectTurnstile = 'IndirectTurnstile'; /// Fichier du tourniquet indirect
+  fOutside = 'Outside';                     /// Fichier du dehors
+
+resourcestring
+  sBlindAlley = 'Impasse';
+
+  sYouCantFly = 'Tu ne peux pas voler !';
 
 type
   {*
@@ -60,6 +104,95 @@ type
     procedure Entering(Player : TPlayer; OldDirection : TDirection;
       KeyPressed : boolean; Src, Pos : T3DPoint;
       var Cancel, AbortEntered : boolean); override;
+  end;
+
+  {*
+    Eau
+    L'eau est un terrain sur lequel on peut aller avec une bouée ou une barque,
+    et au-dessus duquel on peut passer avec une planche.
+  *}
+  TWater = class(TField)
+  public
+    constructor Create(AMaster : TMaster; const AID : TComponentID;
+      const AName : string; ADelegateDrawTo : TField = nil);
+
+    procedure Entering(Player : TPlayer; OldDirection : TDirection;
+      KeyPressed : boolean; Src, Pos : T3DPoint;
+      var Cancel, AbortEntered : boolean); override;
+  end;
+
+  {*
+    Trou
+    Le trou est un terrain au-dessus duquel on peut passer avec une planche, et
+    sinon inaccessible.
+  *}
+  THole = class(TField)
+  public
+    constructor Create(AMaster : TMaster; const AID : TComponentID;
+      const AName : string; ADelegateDrawTo : TField = nil);
+
+    procedure Entering(Player : TPlayer; OldDirection : TDirection;
+      KeyPressed : boolean; Src, Pos : T3DPoint;
+      var Cancel, AbortEntered : boolean); override;
+  end;
+
+  {*
+    Bloc en argent
+    Le bloc en argent peut être détruit au moyen d'une clef en argent.
+  *}
+  TSilverBlock = class(TField)
+  public
+    constructor Create(AMaster : TMaster; const AID : TComponentID;
+      const AName : string; ADelegateDrawTo : TField = nil);
+
+    procedure Entering(Player : TPlayer; OldDirection : TDirection;
+      KeyPressed : boolean; Src, Pos : T3DPoint;
+      var Cancel, AbortEntered : boolean); override;
+  end;
+
+  {*
+    Bloc en or
+    Le bloc en or peut être détruit au moyen d'une clef en or.
+  *}
+  TGoldenBlock = class(TField)
+  public
+    constructor Create(AMaster : TMaster; const AID : TComponentID;
+      const AName : string; ADelegateDrawTo : TField = nil);
+
+    procedure Entering(Player : TPlayer; OldDirection : TDirection;
+      KeyPressed : boolean; Src, Pos : T3DPoint;
+      var Cancel, AbortEntered : boolean); override;
+  end;
+
+  {*
+    Ciel
+    Le ciel est toujours inaccessible.
+  *}
+  TSky = class(TField)
+  public
+    constructor Create(AMaster : TMaster; const AID : TComponentID;
+      const AName : string; ADelegateDrawTo : TField = nil);
+
+    procedure Entering(Player : TPlayer; OldDirection : TDirection;
+      KeyPressed : boolean; Src, Pos : T3DPoint;
+      var Cancel, AbortEntered : boolean); override;
+  end;
+
+  {*
+    Flèche (de toutes directions)
+    Les flèches repoussent le joueur dans la direction qui leur est propre.
+  *}
+  TArrow = class(TEffect)
+  private
+    FDirection : TDirection; /// Direction de la flèche
+  public
+    constructor Create(AMaster : TMaster; const AID : TComponentID;
+      const AName : string; ADirection : TDirection);
+
+    procedure Entered(Player : TPlayer; KeyPressed : boolean;
+      Src, Pos : T3DPoint; var GoOnMoving : boolean); override;
+
+    property Direction : TDirection read FDirection;
   end;
 
   {*
@@ -121,11 +254,23 @@ begin
   // Terrains
   TGrass.Create(Master, idGrass, sGrass);
   TWall.Create(Master, idWall, sWall);
+  TWater.Create(Master, idWater, sWater);
+  THole.Create(Master, idHole, sHole);
+  TSilverBlock.Create(Master, idSilverBlock, sSilverBlock);
+  TGoldenBlock.Create(Master, idGoldenBlock, sGoldenBlock);
+  TSky.Create(Master, idSky, sSky);
 
   // Effets de case
   TEffect.Create(Master, idNoEffect, '');
+
+  TArrow.Create(Master, idNorthArrow, sNorthArrow, diNorth);
+  TArrow.Create(Master, idEastArrow , sEastArrow , diEast );
+  TArrow.Create(Master, idSouthArrow, sSouthArrow, diSouth);
+  TArrow.Create(Master, idWestArrow , sWestArrow , diWest );
+
   TDirectTurnstile.Create(Master, idDirectTurnstile, sDirectTurnstile);
   TIndirectTurnstile.Create(Master, idIndirectTurnstile, sIndirectTurnstile);
+
   TOutside.Create(Master, idOutside, sOutside);
 end;
 
@@ -184,6 +329,246 @@ procedure TWall.Entering(Player : TPlayer; OldDirection : TDirection;
   var Cancel, AbortEntered : boolean);
 begin
   Cancel := True;
+end;
+
+/////////////////////
+/// Classe TWater ///
+/////////////////////
+
+{*
+  Crée une instance de TWater
+  @param AMaster           Maître FunLabyrinthe
+  @param AID               ID du terrain
+  @param AName             Nom du terrain
+  @param ADelegateDrawTo   Un autre terrain auquel déléguer l'affichage
+*}
+constructor TWater.Create(AMaster : TMaster; const AID : TComponentID;
+  const AName : string; ADelegateDrawTo : TField = nil);
+begin
+  inherited Create(AMaster, AID, AName, ADelegateDrawTo);
+  Painter.ImgNames.Add(fWater);
+end;
+
+{*
+  Exécuté lorsque le joueur tente de venir sur la case
+  Entering est exécuté lorsque le joueur tente de venir sur la case. Pour
+  annuler le déplacement, il faut positionner Cancel à True. Pour éviter que
+  la méthode Entered de la case ne soit exécutée, il faut positionner
+  AbortEntered à True.
+  @param Player         Joueur qui se déplace
+  @param OldDirection   Direction du joueur avant ce déplacement
+  @param KeyPressed     True si une touche a été pressée pour le déplacement
+  @param Src            Case de provenance
+  @param Pos            Position de la case
+  @param Cancel         À positionner à True pour annuler le déplacement
+  @param AbortEntered   À positionner à True pour empêcher le Entered
+*}
+procedure TWater.Entering(Player : TPlayer; OldDirection : TDirection;
+  KeyPressed : boolean; Src, Pos : T3DPoint;
+  var Cancel, AbortEntered : boolean);
+begin
+  { TODO 1 : Implémenter TWater.Entering }
+  Cancel := True;
+end;
+
+////////////////////
+/// Classe THole ///
+////////////////////
+
+{*
+  Crée une instance de THole
+  @param AMaster           Maître FunLabyrinthe
+  @param AID               ID du terrain
+  @param AName             Nom du terrain
+  @param ADelegateDrawTo   Un autre terrain auquel déléguer l'affichage
+*}
+constructor THole.Create(AMaster : TMaster; const AID : TComponentID;
+  const AName : string; ADelegateDrawTo : TField = nil);
+begin
+  inherited Create(AMaster, AID, AName, ADelegateDrawTo);
+  Painter.ImgNames.Add(fHole);
+end;
+
+{*
+  Exécuté lorsque le joueur tente de venir sur la case
+  Entering est exécuté lorsque le joueur tente de venir sur la case. Pour
+  annuler le déplacement, il faut positionner Cancel à True. Pour éviter que
+  la méthode Entered de la case ne soit exécutée, il faut positionner
+  AbortEntered à True.
+  @param Player         Joueur qui se déplace
+  @param OldDirection   Direction du joueur avant ce déplacement
+  @param KeyPressed     True si une touche a été pressée pour le déplacement
+  @param Src            Case de provenance
+  @param Pos            Position de la case
+  @param Cancel         À positionner à True pour annuler le déplacement
+  @param AbortEntered   À positionner à True pour empêcher le Entered
+*}
+procedure THole.Entering(Player : TPlayer; OldDirection : TDirection;
+  KeyPressed : boolean; Src, Pos : T3DPoint;
+  var Cancel, AbortEntered : boolean);
+begin
+  { TODO 1 : Implémenter THole.Entering }
+  Cancel := True;
+end;
+
+///////////////////////////
+/// Classe TSilverBlock ///
+///////////////////////////
+
+{*
+  Crée une instance de TSilverBlock
+  @param AMaster           Maître FunLabyrinthe
+  @param AID               ID du terrain
+  @param AName             Nom du terrain
+  @param ADelegateDrawTo   Un autre terrain auquel déléguer l'affichage
+*}
+constructor TSilverBlock.Create(AMaster : TMaster; const AID : TComponentID;
+  const AName : string; ADelegateDrawTo : TField = nil);
+begin
+  inherited Create(AMaster, AID, AName, ADelegateDrawTo);
+  Painter.ImgNames.Add(fSilverBlock);
+end;
+
+{*
+  Exécuté lorsque le joueur tente de venir sur la case
+  Entering est exécuté lorsque le joueur tente de venir sur la case. Pour
+  annuler le déplacement, il faut positionner Cancel à True. Pour éviter que
+  la méthode Entered de la case ne soit exécutée, il faut positionner
+  AbortEntered à True.
+  @param Player         Joueur qui se déplace
+  @param OldDirection   Direction du joueur avant ce déplacement
+  @param KeyPressed     True si une touche a été pressée pour le déplacement
+  @param Src            Case de provenance
+  @param Pos            Position de la case
+  @param Cancel         À positionner à True pour annuler le déplacement
+  @param AbortEntered   À positionner à True pour empêcher le Entered
+*}
+procedure TSilverBlock.Entering(Player : TPlayer; OldDirection : TDirection;
+  KeyPressed : boolean; Src, Pos : T3DPoint;
+  var Cancel, AbortEntered : boolean);
+begin
+  Cancel := True;
+end;
+
+///////////////////////////
+/// Classe TGoldenBlock ///
+///////////////////////////
+
+{*
+  Crée une instance de TGoldenBlock
+  @param AMaster           Maître FunLabyrinthe
+  @param AID               ID du terrain
+  @param AName             Nom du terrain
+  @param ADelegateDrawTo   Un autre terrain auquel déléguer l'affichage
+*}
+constructor TGoldenBlock.Create(AMaster : TMaster; const AID : TComponentID;
+  const AName : string; ADelegateDrawTo : TField = nil);
+begin
+  inherited Create(AMaster, AID, AName, ADelegateDrawTo);
+  Painter.ImgNames.Add(fGoldenBlock);
+end;
+
+{*
+  Exécuté lorsque le joueur tente de venir sur la case
+  Entering est exécuté lorsque le joueur tente de venir sur la case. Pour
+  annuler le déplacement, il faut positionner Cancel à True. Pour éviter que
+  la méthode Entered de la case ne soit exécutée, il faut positionner
+  AbortEntered à True.
+  @param Player         Joueur qui se déplace
+  @param OldDirection   Direction du joueur avant ce déplacement
+  @param KeyPressed     True si une touche a été pressée pour le déplacement
+  @param Src            Case de provenance
+  @param Pos            Position de la case
+  @param Cancel         À positionner à True pour annuler le déplacement
+  @param AbortEntered   À positionner à True pour empêcher le Entered
+*}
+procedure TGoldenBlock.Entering(Player : TPlayer; OldDirection : TDirection;
+  KeyPressed : boolean; Src, Pos : T3DPoint;
+  var Cancel, AbortEntered : boolean);
+begin
+  Cancel := True;
+end;
+
+///////////////////
+/// Classe TSky ///
+///////////////////
+
+{*
+  Crée une instance de TSky
+  @param AMaster           Maître FunLabyrinthe
+  @param AID               ID du terrain
+  @param AName             Nom du terrain
+  @param ADelegateDrawTo   Un autre terrain auquel déléguer l'affichage
+*}
+constructor TSky.Create(AMaster : TMaster; const AID : TComponentID;
+  const AName : string; ADelegateDrawTo : TField = nil);
+begin
+  inherited Create(AMaster, AID, AName, ADelegateDrawTo);
+  Painter.ImgNames.Add(fSky);
+end;
+
+{*
+  Exécuté lorsque le joueur tente de venir sur la case
+  Entering est exécuté lorsque le joueur tente de venir sur la case. Pour
+  annuler le déplacement, il faut positionner Cancel à True. Pour éviter que
+  la méthode Entered de la case ne soit exécutée, il faut positionner
+  AbortEntered à True.
+  @param Player         Joueur qui se déplace
+  @param OldDirection   Direction du joueur avant ce déplacement
+  @param KeyPressed     True si une touche a été pressée pour le déplacement
+  @param Src            Case de provenance
+  @param Pos            Position de la case
+  @param Cancel         À positionner à True pour annuler le déplacement
+  @param AbortEntered   À positionner à True pour empêcher le Entered
+*}
+procedure TSky.Entering(Player : TPlayer; OldDirection : TDirection;
+  KeyPressed : boolean; Src, Pos : T3DPoint;
+  var Cancel, AbortEntered : boolean);
+begin
+  if KeyPressed then
+    Player.ShowDialog(sBlindAlley, sYouCantFly, dtError);
+  Cancel := True;
+end;
+
+/////////////////////
+/// Classe TArrow ///
+/////////////////////
+
+{*
+  Crée une instance de TArrow
+  @param AMaster      Maître FunLabyrinthe
+  @param AID          ID de l'effet de case
+  @param AName        Nom de la case
+  @param ADirection   Direction de la flèche
+*}
+constructor TArrow.Create(AMaster : TMaster; const AID : TComponentID;
+  const AName : string; ADirection : TDirection);
+begin
+  inherited Create(AMaster, AID, AName);
+  FDirection := ADirection;
+  case FDirection of
+    diNorth : Painter.ImgNames.Add(fNorthArrow);
+    diEast  : Painter.ImgNames.Add(fEastArrow);
+    diSouth : Painter.ImgNames.Add(fSouthArrow);
+    diWest  : Painter.ImgNames.Add(fWestArrow);
+  end;
+end;
+
+{*
+  Exécuté lorsque le joueur est arrivé sur la case
+  Entered est exécuté lorsque le joueur est arrivé sur la case.
+  @param Player       Joueur qui se déplace
+  @param KeyPressed   True si une touche a été pressée pour le déplacement
+  @param Src          Case de provenance
+  @param Pos          Position de la case
+  @param GoOnMoving   À positionner à True pour réitérer le déplacement
+*}
+procedure TArrow.Entered(Player : TPlayer; KeyPressed : boolean;
+  Src, Pos : T3DPoint; var GoOnMoving : boolean);
+begin
+  inherited;
+  Player.Direction := FDirection;
+  GoOnMoving := True;
 end;
 
 ///////////////////////////////
