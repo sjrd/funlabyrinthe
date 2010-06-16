7aa657e326687d8416b186753b136435a4a5daba
Sébastien Doeraene <sjrdoeraene@gmail.com>
2010-06-16 09:48:08 +0000

    Suppression de la licence SynEdit dans SynHighlighterFunDelphi.pas.

--- EditPlugins/FunDelphiSourceEditor/SynHighlighterFunDelphi.pas
+++ EditPlugins/FunDelphiSourceEditor/SynHighlighterFunDelphi.pas
@@ -1,39 +1,3 @@
-{-------------------------------------------------------------------------------
-The contents of this file are subject to the Mozilla Public License
-Version 1.1 (the "License"); you may not use this file except in compliance
-with the License. You may obtain a copy of the License at
-http://www.mozilla.org/MPL/
-
-Software distributed under the License is distributed on an "AS IS" basis,
-WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
-the specific language governing rights and limitations under the License.
-
-Code template generated with SynGen.
-The original code is: C:\Projets\FunLabyrinthe\EditPlugins\FunDelphiSourceEditor\SynEditFunDelphiGrammar.pas, released 2010-06-13.
-Description: Syntax Parser/Highlighter
-The initial author of this file is Sepi.
-Copyright (c) 2010, all rights reserved.
-
-Contributors to the SynEdit and mwEdit projects are listed in the
-Contributors.txt file.
-
-Alternatively, the contents of this file may be used under the terms of the
-GNU General Public License Version 2 or later (the "GPL"), in which case
-the provisions of the GPL are applicable instead of those above.
-If you wish to allow use of your version of this file only under the terms
-of the GPL and not to allow others to use your version of this file
-under the MPL, indicate your decision by deleting the provisions above and
-replace them with the notice and other provisions required by the GPL.
-If you do not delete the provisions above, a recipient may use your version
-of this file under either the MPL or the GPL.
-
-$Id: $
-
-You may retrieve the latest version of this file at the SynEdit home page,
-located at http://SynEdit.SourceForge.net
-
--------------------------------------------------------------------------------}
-
 {$IFNDEF QSYNEDITFUNDELPHIGRAMMAR}
 unit SynHighlighterFunDelphi;
 {$ENDIF}
