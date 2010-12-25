ac8b01026627d6c081ac78850e28431306db0592
Sébastien Doeraene <sjrdoeraene@gmail.com>
2010-12-25 17:43:51 +0000

    Mise à jour du batch pour madExcept.

--- PatchWithMadExcept.bat
+++ PatchWithMadExcept.bat
@@ -1,8 +1,9 @@
-madExceptPatch FunLabyCore.bpl
-madExceptPatch FunLabyTools.bpl
-madExceptPatch FunLabyForms.bpl
-madExceptPatch FunLabyEditPlugin.bpl
-madExceptPatch FunLabyEditTools.bpl
-madExceptPatch FunLaby.exe
-madExceptPatch FunLabyEdit.exe
-madExceptPatch GeneLaby.exe
+madExceptPatch FunLabyCore.bpl FunLabyrinthe.mes
+madExceptPatch FunLabyTools.bpl FunLabyrinthe.mes
+madExceptPatch FunLabyForms.bpl FunLabyrinthe.mes
+madExceptPatch FunLabyEditPlugin.bpl FunLabyrinthe.mes
+madExceptPatch FunLabyEditTools.bpl FunLabyrinthe.mes
+madExceptPatch FunLabyRepo.bpl FunLabyrinthe.mes
+madExceptPatch FunLaby.exe FunLabyrinthe.mes
+madExceptPatch FunLabyEdit.exe FunLabyrinthe.mes
+madExceptPatch GeneLaby.exe FunLabyrinthe.mes
