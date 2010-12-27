893805edaa4d3d1a07dac58afbe9645c17471da1
Sébastien Doeraene <sjrdoeraene@gmail.com>
2010-12-27 17:58:00 +0000

    Ajout de l'unité principale FunLabyRepo.pas dans le package FunLabyRepository.

---
+++ Packages/FunLabyRepository/FunLabyRepo.pas
@@ -0,0 +1,16 @@
+{*
+  Gestion du repository de FunLabyrinthe
+  @author sjrd
+  @version 5.0
+*}
+unit FunLabyRepo;
+
+interface
+
+uses
+  Windows, SysUtils, Classes, FunLabyUtils;
+
+implementation
+
+end.
+
