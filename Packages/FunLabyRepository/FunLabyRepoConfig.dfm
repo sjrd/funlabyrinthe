5e6e64c35f4b95676af93e7d5a88e42719f203d6
Sébastien Doeraene <sjrdoeraene@gmail.com>
2011-01-26 19:15:52 +0100

    Ajout d'une fenêtre pour la configuration du repository.

---
+++ Packages/FunLabyRepository/FunLabyRepoConfig.dfm
@@ -0,0 +1,18 @@
+object FormRepoConfig: TFormRepoConfig
+  Left = 0
+  Top = 0
+  Caption = 'Configuration de votre repository'
+  ClientHeight = 313
+  ClientWidth = 457
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poMainFormCenter
+  Scaled = False
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ Packages/FunLabyRepository/FunLabyRepoConfig.pas
@@ -0,0 +1,32 @@
+{*
+  Fenêtre de configuration du repository
+  @author sjrd
+  @version 5.2
+*}
+unit FunLabyRepoConfig;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  {*
+    Fenêtre de configuration du repository
+    @author sjrd
+    @version 5.2
+  *}
+  TFormRepoConfig = class(TForm)
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+end.
+
